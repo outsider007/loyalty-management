e101953ad16f99433cd4b0aa006d312297a4f687
outsider007 <nikolai.kuznetsov.nk@gmail.com>
2021-05-23 16:01:28 +0700

    Initial commit

---
+++ src/main/java/ru/kuznetsov/loyaltymanagement/LoyaltyManagementApplication.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.loyaltymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyaltyManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kuznetsov/loyaltymanagement/LoyaltyManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.loyaltymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
