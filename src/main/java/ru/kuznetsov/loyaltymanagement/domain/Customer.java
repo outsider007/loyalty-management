f5e0dcd832dcd4c384d34dcb09de8180ab1460d0
outsider007 <nikolai.kuznetsov.nk@gmail.com>
2021-05-23 16:44:45 +0700

    Entity has been added

---
+++ src/main/java/ru/kuznetsov/loyaltymanagement/domain/Customer.java
@@ -0,0 +1,30 @@
+package ru.kuznetsov.loyaltymanagement.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "middle_name")
+    private String middleName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "birthday")
+    private Date birthday;
+    @Column(name = "registered_date")
+    private Date registeredDate;
+}
