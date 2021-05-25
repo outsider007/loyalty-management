ee137ccf4745e2c16e7c412962604f62d54ad617
outsider007 <nikolai.kuznetsov.nk@gmail.com>
2021-05-26 02:12:26 +0700

    Переработана структура проекта

--- loyaltymanagement-web/src/main/java/ru/kuznetsov/loyaltymanagement/web/repositories/BalanceChangeRepository.java
+++ loyaltymanagement-web/src/main/java/ru/kuznetsov/loyaltymanagement/web/repositories/BalanceChangeRepository.java
@@ -1,8 +1,8 @@
-package ru.kuznetsov.loyaltymanagement.repositories;
+package ru.kuznetsov.loyaltymanagement.web.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.kuznetsov.loyaltymanagement.domain.BalanceChange;
+import ru.kuznetsov.loyaltymanagement.web.domain.BalanceChange;
 
 @Repository
 public interface BalanceChangeRepository extends JpaRepository<BalanceChange, Integer> {
