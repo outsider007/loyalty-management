8b85091a9dc27175356cee9d7f8672c1d72cdaed
outsider007 <nikolai.kuznetsov.nk@gmail.com>
2021-05-29 21:20:41 +0700

    Added new sql-request for select customer from phone number

--- src/main/java/ru/kuznetsov/loyaltymanagement/dao/repositories/CustomerRepository.java
+++ src/main/java/ru/kuznetsov/loyaltymanagement/dao/repositories/CustomerRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    List<Customer> findByFirstNameStartingWith(String firstName);
+
+    List<Customer> findByPhoneNumberStartingWithOrFirstNameStartingWith(String filter, String filtrate);
 }
